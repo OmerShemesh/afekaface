f5b4fee597ef0ae1cdf39469f3f8261a1a5b5e74
Omer Shemsheyan <omershemesh95@gmail.com>
2016-08-04 17:27:42 +0300

    ...

--- src/app/notfound/notfound.component.ts
+++ src/app/notfound/notfound.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   template:
     `<div class="container col-md-5 col-md-offset-4">
       <h1>404 Not Found</h1>
-      <a style="margin-left:100px" title="Go Back Home" href="/"><i class="fa fa-home fa-3x"></i><i class="fa fa-hand-o-left fa-3x" ></i></a>
     </div>`,
   styleUrls: ['notfound.component.css']
 })
